e642583d9709c4e16eb436b187897084a0d45c1e
Annélise Rowan <40217057+AnneliseRowan@users.noreply.github.com>
2021-08-28 13:38:21 -0400

    worked one file

---
+++ client/src/pages/SavedPlants.js
@@ -0,0 +1,94 @@
+import React from 'react';
+import {
+  Jumbotron,
+  Container,
+  CardColumns,
+  Card,
+  Button,
+} from 'react-bootstrap';
+
+import { useQuery, useMutation } from '@apollo/client';
+import { QUERY_ME } from '../utils/queries';
+import { REMOVE_PLANT } from '../utils/mutations';
+import { removePlantId } from '../utils/localStorage';
+
+import Auth from '../utils/auth';
+
+const SavedPlants = () => {
+  const { loading, data } = useQuery(QUERY_ME);
+  const [removePlant, { error }] = useMutation(REMOVE_PLANT);
+
+  const userData = data?.me || {};
+
+  // create function that accepts the plants's mongo _id value as param and deletes the plant from the database
+  const handleDeletePlant = async (plantId) => {
+    // get token
+    const token = Auth.loggedIn() ? Auth.getToken() : null;
+
+    if (!token) {
+      return false;
+    }
+
+    try {
+      const { data } = await removePlant({
+        variables: { plantId },
+      });
+
+      // upon success, remove plants's id from localStorage
+      removePlantId(plantId);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  if (loading) {
+    return <h2>LOADING...</h2>;
+  }
+
+  return (
+    <>
+      <Jumbotron fluid className="text-light bg-dark">
+        <Container>
+          <h1>Viewing {userData.username}'s plants!</h1>
+        </Container>
+      </Jumbotron>
+      <Container>
+        <h2>
+          {userData.savedPlants?.length
+            ? `Viewing ${userData.savedPlants.length} saved ${
+                userData.savedPlants.length === 1 ? 'plants' : 'plantss'
+              }:`
+            : 'You have no saved plants!'}
+        </h2>
+        <CardColumns>
+          {userData.savedPlants?.map((plant) => {
+            return (
+              <Card key={plant.plantId} border="dark">
+                {plant.image ? (
+                  <Card.Img
+                    src={plant.image}
+                    alt={`The image for ${plant.name}`}
+                    variant="top"
+                  />
+                ) : null}
+                <Card.Body>
+                  <Card.Title>{plant.name}</Card.Title>
+                  <p className="small">Water: {plant.water}</p>
+                  <Card.Text>{plant.sun} {plant.pet} </Card.Text>
+                  <Button
+                    className="btn-block btn-danger"
+                    onClick={() => handleDeletePlant(plant.plantId)}
+                  >
+                    Delete this Plant!
+                  </Button>
+                </Card.Body>
+              </Card>
+            );
+          })}
+        </CardColumns>
+      </Container>
+    </>
+  );
+};
+
+export default SavedPlants;
