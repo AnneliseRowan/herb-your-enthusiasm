5e4beae29f5a68f149c154d603e35de128f26ebe
Lex <aligon17@gmail.com>
2021-08-28 11:51:51 -0400

    added plant model

---
+++ server/models/Plant.js
@@ -0,0 +1,31 @@
+const { Schema, model } = require('mongoose');
+
+const plantSchema = new Schema({
+    plantName: {
+        type: String,
+        required: true
+    },
+    
+    plantLight: {
+        type: String,
+        required: true
+    },
+
+    plantWater: {
+        type: String,
+        required: true
+    },
+
+    petFriendly: {
+        type: Boolean,
+        required: true
+    },
+
+});
+
+const Plant = model('Plant', plantSchema);
+
+module.exports = Plant
+
+//possibly add plant 
+// plantWater might not be string??
