b2b8617e6b438b8bac590a84f74427a53ce619ab
Lex <aligon17@gmail.com>
2021-09-03 13:55:05 -0400

    added an easy notification to donation page

--- client/src/pages/Donate.js
+++ client/src/pages/Donate.js
@@ -1,13 +1,23 @@
 import React from 'react';
-
+// for toast notifications
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 import Auth from '../utils/auth';
 
 const Donate = () => {
+
+    const notify = () => toast("Wow so easy!");
     return (
-       
-        <h1> Plant a Seed make it grow!</h1>
+        <div>
+            <h1> Plant a Seed make it grow!</h1>
+            <div>
+                <button onClick={notify}> Notify !</button>
+
+                <ToastContainer/>
+            </div>
+        </div>
     )
 }
 
--- client/src/pages/WelcomePage.js
+++ client/src/pages/WelcomePage.js
@@ -3,8 +3,8 @@ import './style.css'
 import Auth from '../utils/auth';
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; 
 
-
 const WelcomePage = () => {
+    
     return (
         <div style={{backgroundColor: "#EEE2DC"}}>
             <div className="bg-image" style={{"backgroundImage": "url('https://images.unsplash.com/photo-1496309732348-3627f3f040ee?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80')", height: "100vh", width: "100vw"}}>
