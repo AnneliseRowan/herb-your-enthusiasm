eb1a98be1af9bb7c6f3abe78702380c58c179c7e
Lex <aligon17@gmail.com>
2021-09-03 14:30:02 -0400

    added some buttons with notifications

--- client/src/pages/Donate.js
+++ client/src/pages/Donate.js
@@ -5,15 +5,30 @@ import 'react-toastify/dist/ReactToastify.css';
 
 
 import Auth from '../utils/auth';
-
+toast.configure()
 const Donate = () => {
+    // notification for when it's time to water your plant
+    const waterNotify = () => {
+        toast.info("Your plant is thirsty, time to water!")
+    };
 
-    const notify = () => toast("Wow so easy!");
+    // notification for after you watered your plant
+    const waterSuccess = () => {
+        toast("Hooray! Your plant is watered")
+    };
+
+    // notification for when a new plant is added to users collection
+    const newPlant = () => {
+        toast.success("Success! You added a new plant to your garden")
+    }
     return (
         <div>
             <h1> Plant a Seed make it grow!</h1>
             <div>
-                <button onClick={notify}> Notify !</button>
+                <button onClick={waterNotify}> Need to water my plant !</button>
+                <button onClick={waterSuccess}> Water my plant !</button>
+                <button onClick={newPlant}> Add a new plant !</button>
+                <button onClick={newPlant}> Notify !</button>
 
                 <ToastContainer/>
             </div>
