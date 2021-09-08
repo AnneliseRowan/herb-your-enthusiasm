996e5f33366908fa9b29b583be681477d44f5401
Annélise Rowan <Annelise.Rowan@gmail.com>
2021-09-08 10:55:10 -0400

    updated saveplants.js page (#44)

--- client/src/pages/SavedPlants.js
+++ client/src/pages/SavedPlants.js
@@ -66,37 +66,39 @@ const SavedPlants = () => {
                     <img style={{width: "160px", height: "160px", borderRadius: "80px"}} 
                     src="https://images.unsplash.com/photo-1523983254932-c7e6571c9d60?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2019&q=80"
                     />
-                    <span style={{fontFamily: 'Oleo Script, cursive', fontSize: "96px", marginLeft: "20px"}}>{userData.name}'s Plant Family</span>
+                    <span style={{fontFamily: 'Oleo Script, cursive', fontSize: "96px", marginLeft: "20px"}}>{user.data.username}'s Plant Family</span>
                 </div>
             </div>
         </div>
       <Container>
         <h2 style={{textAlign: "center", marginTop: "45px"}}>
-          {userData.savedPlants?.length
+          {data.userplants.savedPlants?.length        
             ? `Viewing ${userData.savedPlants.length} saved ${
-                userData.savedPlants.length === 1 ? 'plants' : 'plants'
-              }:`
-            : 'Uh-Oh, Hurry! Adopt some plants!'}
+                userData.length >= 1 ? 'plants' : 'plants'
+              }`:
+             'Uh-Oh, Hurry! Adopt some plants!'}
+            {console.log(userData.length, 'UserData is here!!!')}
         </h2>
         <Row xs={1} md={2} className="g-4">
           <Col >
-            {userData.savedPlants?.map((plant) => {
+            {data.userplants?.map((plant) => {
+              console.log(plant, 'plant data for card')
               return (
-                <Card key={plant.plantId} border="dark">
-                  {plant.image ? (
+                <Card key={plant._id} border="dark">
+                  {plant.plantImage ? (
                     <Card.Img
-                      src={plant.image}
-                      alt={`The image for ${plant.name}`}
+                      src={plant.plantImage}
+                      alt={`The image for ${plant.plantName}`}
                       variant="top"
                     />
                   ) : null}
                   <Card.Body>
-                    <Card.Title>{plant.name}</Card.Title>
-                    <p className="small">Water: {plant.water}</p>
-                    <Card.Text>{plant.light} {plant.pet} </Card.Text>
+                    <Card.Title>{plant.plantName}</Card.Title>
+                    <p className="small">Water: {plant.plantWater}</p>
+                    <Card.Text>{plant.plantLight} {plant.plantPet} </Card.Text>
                     <Button
                       className="btn-block btn-danger"
-                      onClick={() => handleDeletePlant(plant.plantId)}
+                      onClick={() => handleDeletePlant(plant._id)}
                     >
                       Delete this Plant!
                     </Button>
