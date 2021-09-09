f028fbeb0b3f2b135846b1d26ce9d2c2025aec3e
Annélise Rowan <40217057+AnneliseRowan@users.noreply.github.com>
2021-09-08 21:17:32 -0400

    added coloring, got the saved plant cards to populate correctly

--- client/src/pages/Donate.js
+++ client/src/pages/Donate.js
@@ -1,48 +1,18 @@
 import React, { useState } from 'react';
-// for toast notifications
-//import { ToastContainer, toast } from 'react-toastify';
-//import 'react-toastify/dist/ReactToastify.css';
 import StripeContainer from '../components/StripeContainer';
-//import DateTime from '../components/Time';
 import './Style.css'; 
 
-
-//import Auth from '../utils/auth';
-//toast.configure()
 const Donate = () => {
     const [showItem, setShowItem] = useState(false)
     return (
-      <div className="Donate" style={{marginBottom: "50px", marginTop: "30px"}}>
-        <h1>Donate and Plant a Seed</h1>
-        {showItem ? <StripeContainer/> : <> <h3>$5.00</h3> <img src="https://images.unsplash.com/photo-1459156212016-c812468e2115?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1898&q=80" alt="small plant" style={{display: "block", marginLeft: "auto", marginRight: "auto", width: "50%"}} />
-        <button onClick={() => setShowItem(true)}>Donate</button></>}
-        <h2>Look at our Github Page</h2>
+      <div className="Donate" style={{marginBottom: "50px", marginTop: "30px", textAlign: "center"}}>
+        <h1 style={{fontFamily: 'Oleo Script, cursive', fontSize: "64px"}}>Donate and Plant a Seed</h1>
+        {showItem ? <StripeContainer/> : <> <h3 style={{fontSize: "32px"}}>$5.00</h3> <img src="https://images.unsplash.com/photo-1459156212016-c812468e2115?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1898&q=80" alt="small plant" style={{display: "block", marginLeft: "auto", marginRight: "auto", width: "50%"}} />
+        <button onClick={() => setShowItem(true)} style={{marginBottom: "20px"}}>Donate</button></>}
+        <span style={{fontSize: "48px", fontFamily: 'Oleo Script, cursive'}}>Visit Our <a href="https://github.com/AnneliseRowan/herb-your-enthusiam" target="blank">Github</a></span> 
       </div>
     );
 
 }
 
 export default Donate;
-
-    // // notification for when it's time to water your plant
-    // const waterNotify = () => {
-    //     toast.info("Your plant is thirsty, time to water!")
-    // };
-
-    // // notification for when a new plant is added to users collection
-    // const newPlant = () => {
-    //     toast.success("Success! You added a new plant to your garden")
-    // }
-    // return (
-    //     <div>
-    //         <h1> Plant a Seed make it grow!</h1>
-    //         <div>
-    //             <button onClick={waterNotify}> Need to water my plant !</button>
-    //             <button onClick={newPlant}> Add a new plant !</button>
-
-    //             <ToastContainer/>
-    //             <DateTime/>
-    //         </div>
-    //         <div>
-    //         </div>
-    //     </div>
--- client/src/pages/SavedPlants.js
+++ client/src/pages/SavedPlants.js
@@ -14,6 +14,8 @@ import { QUERY_ME, QUERY_USER_PLANT } from '../utils/queries';
 import { REMOVE_PLANT } from '../utils/mutations';
 import { removePlantId } from '../utils/localStorage';
 
+import './Style.css'
+
 import Auth from '../utils/auth';
 
 const SavedPlants = () => {
@@ -22,10 +24,8 @@ const SavedPlants = () => {
   const [removePlant, { error }] = useMutation(REMOVE_PLANT);
   
   const userData = data?.userplants || {};
-  
-  console.log(data, 'wheres the data')
+
   const user = Auth.getProfile()
-  console.log(user, 'user!!')
 
   const handleDeletePlant = async (plantId) => {
     const token = Auth.loggedIn() ? Auth.getToken() : null;
@@ -53,62 +53,55 @@ const SavedPlants = () => {
   return (
     <>
       <div>
-            <div className="bg-light" style={{
-                display: "flex",
-                // justifyContent: "space-between",
-                // alignContent: "center",
-                // margin: "20px 0px 0px 0px",
-                paddingBottom: "20px",
-                marginTop: "45px"
-                // borderBottom: "3px solid grey"
-            }}> 
-                <div style={{marginLeft: "15%"}}>
-                    <img style={{width: "160px", height: "160px", borderRadius: "80px"}} 
-                    src="https://images.unsplash.com/photo-1523983254932-c7e6571c9d60?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2019&q=80"
-                    />
-                    <span style={{fontFamily: 'Oleo Script, cursive', fontSize: "96px", marginLeft: "20px"}}>{user.data.username}'s Plant Family</span>
-                </div>
-            </div>
+          <div style={{
+            backgroundColor: "#C2CAD0",
+            display: "flex",
+            paddingBottom: "20px",
+            marginTop: "45px"
+          }}> 
+              <div style={{marginLeft: "15%", paddingTop: "30px", paddingBottom: "5px"}}>
+                  <img style={{width: "190px", height: "190px", borderRadius: "80px"}} 
+                  src="https://images.unsplash.com/photo-1523983254932-c7e6571c9d60?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2019&q=80"
+                  />
+                  <span style={{fontFamily: 'Oleo Script, cursive', fontSize: "96px", marginLeft: "20px"}}>{user.data.username}'s Plant Family</span>
+              </div>
+          </div>
         </div>
       <Container>
         <h2 style={{textAlign: "center", marginTop: "45px"}}>
-          {data.userplants.savedPlants?.length        
-            ? `Viewing ${userData.savedPlants.length} saved ${
-                userData.length >= 1 ? 'plants' : 'plants'
-              }`:
+          {userData.length        
+            ? `See Your Garden!`:
              'Uh-Oh, Hurry! Adopt some plants!'}
-            {console.log(userData.length, 'UserData is here!!!')}
         </h2>
-        <Row xs={1} md={2} className="g-4">
-          <Col >
-            {data.userplants?.map((plant) => {
-              console.log(plant, 'plant data for card')
-              return (
-                <Card key={plant._id} border="dark">
-                  {plant.plantImage ? (
-                    <Card.Img
-                      src={plant.plantImage}
-                      alt={`The image for ${plant.plantName}`}
-                      variant="top"
-                    />
-                  ) : null}
-                  <Card.Body>
-                    <Card.Title>{plant.plantName}</Card.Title>
-                    <p className="small">Water: {plant.plantWater}</p>
-                    <Card.Text>{plant.plantLight} {plant.plantPet} </Card.Text>
-                    <Button
-                      className="btn-block btn-danger"
-                      onClick={() => handleDeletePlant(plant._id)}
-                    >
-                      Delete this Plant!
-                    </Button>
-                  </Card.Body>
-                </Card>
-              );
-            })}
-          </Col>
-        </Row>
       </Container>
+        <Row xs={1} md={2} lg={4} >
+          {data.userplants?.map((plant) => (
+            <Col key={plant._id}>
+              <Card  border="light" style={{width: "24rem", margin:"10px", padding: "0px 10px 0px 10px"}} id="cardSizing">
+                {plant.plantImage ? (
+                  <Card.Img style={{height:"36rem"}}
+                    src={plant.plantImage}
+                    alt={`The image for ${plant.plantName}`}
+                    variant="top"
+                  />
+                ) : null}
+                <Card.Body>
+                  <Card.Title style={{fontFamily: 'Oleo Script, cursive', fontSize:"32px", textAlign:"center"}}>{plant.plantName}</Card.Title>
+                  <p className="medium">Sun: {plant.plantLight}</p>
+                  <p className="medium">Water: {plant.plantWater}</p>
+                  {/* <p className="medium">Pet-Friendly: {checkTrue(plant.petFriendly)}</p> */}
+                  <Button
+                    className="btn-block"
+                    style={{backgroundColor: "#88BDBC"}}
+                    onClick={() => handleDeletePlant(plant._id)}
+                  >
+                    Adios Plant!
+                  </Button>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Row>
     </>
   );
 };
