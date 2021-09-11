e9b061875d662949fff6accce839f507d33c313d
Jon Nance <jonathan.w.nance@gmail.com>
2021-09-10 20:39:17 -0400

    water me! Button will turn read if water is needed (#68)

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -9,13 +9,11 @@ const expiration = '2h';
 module.exports = {
   authMiddleware: function ({ req }) {
     // allows token to be sent via req.body, req.query, or headers
-    let token = req.body.token || req.query.token || req.headers.authorization;
-    console.log("token", token)
+    let token = req.body.token || req.query.token || req.headers.authorization;    
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
       token = token.split(' ').pop().trim();
     }
-    console.log('token:', token)
 
     if (!token) {
       return req;
@@ -23,12 +21,10 @@ module.exports = {
 
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
-      console.log('data:', data)
       req.user = data;
     } catch {
       console.log('Invalid token');
     }
-    console.log('req: user', req.user)
 
     return req;
   },
